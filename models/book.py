65cd41f52a67865c0be8d93fd9411c8ae843edcb
kivenixx <olivier206260@gmail.com>
2026-07-11 20:22:20 +0200

    Add input validation for numeric fields

--- models/book.py
+++ models/book.py
@@ -1,19 +1,19 @@
 class Book:
     def __init__(self):
-        self.author = str(input("Podaj autora: "))
-        self.title = str(input("Podaj tytuł: "))
-        self.year = int(input("Podaj rok wydania: "))
-        self.length = int(input("Podaj ilość stron: "))
-        self.font_size = float(input("Podaj wielkość czcionki: "))
-        self.weight = float(input("Podaj wagę: "))
+        self.author = input("Podaj autora: ")
+        self.title = input("Podaj tytuł: ")
+
+        self.year = validation("Podaj rok wydania: ", "i")
+        self.length = validation("Podaj ilość stron: ", "i")
+        self.font_size = validation("Podaj wielkość czcionki: ", "f")
+        self.weight = validation("Podaj wagę: ", "f")
 
         while True:
             resp = input("Czy jest lekturą szkolną (t/n): ").lower()
 
             if resp in(["t","n"]): break
 
-        self.school_required = resp
-        print(self.school_required)
+        self.school_required = True if resp == "t" else False
 
     def display(self):
         print(
@@ -26,3 +26,18 @@ class Book:
             f"Lektura szkolna: {"Tak" if self.school_required else "Nie"}"
         )
 
+
+def validation(message, var_type):
+    while True:
+        resp = input(message)
+
+        try:
+            if var_type == "i":
+                attribute = int(resp)
+            elif var_type == "f":
+                attribute = float(resp)
+            else:
+                raise SystemExit("Error: Variable type not allowed.")
+            return attribute
+        except ValueError:
+            print("Podana wartość nie jest liczbą całkowitą.")
