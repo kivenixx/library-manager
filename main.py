b2aa99d4e9e1b26e8d8e05c6058d786d911841fb
kivenixx <olivier206260@gmail.com>
2026-07-11 18:01:44 +0200

    Add initial interactive menu (main.py)

---
+++ main.py
@@ -0,0 +1,24 @@
+while True:
+    print(
+        "\n1. Dodaj książkę\n"
+        "2. Wyświetl książki\n"
+        "3. Zapisz do JSON\n"
+        "4. Wczytaj JSON"
+    )
+
+    resp = input()
+
+    if resp == "1":
+        pass
+        #dodaj
+    elif resp == "2":
+        pass
+        #wyswietl
+    elif resp == "3":
+        pass
+        #zapisz
+    elif resp == "4":
+        pass
+        #wczytaj
+    else:
+        print("Wprowadź liczbę od 1 do 4.")
